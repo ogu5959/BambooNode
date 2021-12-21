9e0e27b37bf1523a8543929eed0122b34dcb21ab
ogu5959 <ogu5959@gmail.com>
2021-12-22 00:53:26 +0900

    View Engine

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const agent   = require('./routes/agent')
+const express  = require('express')
+const nunjucks = require('nunjucks')
+const agent    = require('./routes/agent')
 
-const app     = express()
+const app      = express()
+
+nunjucks.configure('template', {
+    autoescape : true,  // 
+    express : app       // app 객체 연결
+})
 
 app.use('/agent', agent)
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!!')
+    res.send('Hello Express!')
 })
 
 app.listen(3000, () => {
--- routes/agent.js
+++ routes/agent.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const router  = express.Router()
+const express  = require('express')
+const router   = express.Router()
+const nunjucks = require('nunjucks')
 
 router.get('/', (req, res) => {
     res.send('agent')
 });
 
 router.get('/list', (req, res) => {
-    res.send('agent list')
+    res.render( 'agent/list.html', {
+        message : 'Hello!'
+    })
 });
 
 module.exports = router;
